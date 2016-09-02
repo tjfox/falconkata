ea103a7bdac8ba6939db8eb114cd5eaf5361e46f
Tasewell Fox <tfox@pillartechnology.com>
2016-09-02 08:14:45 -0500

    Hold off on last test, first test invalid ordering of numeral

--- tests/checkRomanNumeralCalculator.c
+++ tests/checkRomanNumeralCalculator.c
@@ -84,9 +84,9 @@ START_TEST(test_add_invalid_numeral_returns_INVALID_PARAM)
 }
 END_TEST
 
-START_TEST(test_add_IIII_returns_INVALID_PARAM)
+START_TEST(test_invalid_numeral_ordering_returns_INVALID_PARAM)
 {
-  const char * operand1 = "IIII";
+  const char * operand1 = "IIIV";
   const char * operand2 = "I";
   char result[10];
 
@@ -95,6 +95,18 @@ START_TEST(test_add_IIII_returns_INVALID_PARAM)
   ck_assert_int_eq(success, INVALID_PARAM);
 }
 END_TEST
+
+// START_TEST(test_add_IIII_returns_INVALID_PARAM)
+// {
+//   const char * operand1 = "IIII";
+//   const char * operand2 = "I";
+//   char result[10];
+//
+//   int success = add_roman_numeral(operand1, operand2, result);
+//
+//   ck_assert_int_eq(success, INVALID_PARAM);
+// }
+// END_TEST
 
 Suite * calculator_suite(void)
 {
@@ -111,7 +123,8 @@ Suite * calculator_suite(void)
   tcase_add_test(tc_core, test_successful_add_returns_OK);
   tcase_add_test(tc_core, test_add_lower_case_operand_returns_INVALID_PARAM);
   tcase_add_test(tc_core, test_add_invalid_numeral_returns_INVALID_PARAM);
-  tcase_add_test(tc_core, test_add_IIII_returns_INVALID_PARAM);
+  tcase_add_test(tc_core, test_invalid_numeral_ordering_returns_INVALID_PARAM);
+  // tcase_add_test(tc_core, test_add_IIII_returns_INVALID_PARAM);
   suite_add_tcase(s, tc_core);
 
   return s;
