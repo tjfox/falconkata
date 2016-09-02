a760987e264462926e2afa377f142e847d2164eb
Tasewell Fox <tfox@pillartechnology.com>
2016-09-02 10:00:57 -0500

    red test D+M=MD

--- tests/checkRomanNumeralCalculator.c
+++ tests/checkRomanNumeralCalculator.c
@@ -93,6 +93,17 @@ START_TEST(test_C_plus_D_equals_DC)
 }
 END_TEST
 
+START_TEST(test_D_plus_M_equals_MD)
+{
+  const char* operand1 = "D";
+  const char* operand2 = "M";
+  char result[10];
+  int success = add_roman_numeral(operand1, operand2, result);
+
+  ck_assert_str_eq(result, "MD");
+}
+END_TEST
+
 
 START_TEST(test_successful_add_returns_OK)
 {
@@ -181,6 +192,7 @@ Suite * calculator_suite(void)
   tcase_add_test(tc_core, test_X_plus_L_equals_LX);
   tcase_add_test(tc_core, test_L_plus_C_equals_CL);
   tcase_add_test(tc_core, test_C_plus_D_equals_DC);
+  tcase_add_test(tc_core, test_D_plus_M_equals_MD);
   tcase_add_test(tc_core, test_successful_add_returns_OK);
   tcase_add_test(tc_core, test_add_lower_case_operand_returns_INVALID_PARAM);
   tcase_add_test(tc_core, test_add_invalid_numeral_returns_INVALID_PARAM);
