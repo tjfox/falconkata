e753866d24c242d38bd1ee1ab13384f91c073d0f
Tasewell Fox <tfox@pillartechnology.com>
2016-09-01 21:43:42 -0500

    Green test for successful retval

--- src/romanNumeralCalculator.c
+++ src/romanNumeralCalculator.c
@@ -1,7 +1,9 @@
+#include "romanNumeralCalculator.h"
 #include <string.h>
 
-void add_roman_numeral(const char* operand1,
+int add_roman_numeral(const char* operand1,
   const char* operand2, char* result)
   {
     strcpy(result, "II");
+    return OK;
   }
