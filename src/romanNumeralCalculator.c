7f7bde0515c2d71a1a136439c4b1d7bd55284bd5
Tasewell Fox <tfox@pillartechnology.com>
2016-09-01 22:12:26 -0500

    Green test for lowercase parameter retval

--- src/romanNumeralCalculator.c
+++ src/romanNumeralCalculator.c
@@ -1,9 +1,32 @@
 #include "romanNumeralCalculator.h"
+#include <ctype.h>
 #include <string.h>
+
+int lowercase_string_check(const char* operand)
+{
+  int i;
+  for(i = 0; operand[i] != '\0'; ++i)
+  {
+    if(islower(operand[i]))
+    {
+      return INVALID_PARAM;
+    }
+  }
+  return OK;
+}
 
 int add_roman_numeral(const char* operand1,
   const char* operand2, char* result)
   {
+    if(lowercase_string_check(operand1) != OK)
+    {
+      return INVALID_PARAM;
+    }
+    else if(lowercase_string_check(operand2) != OK)
+    {
+      return INVALID_PARAM;
+    }
+
     strcpy(result, "II");
     return OK;
   }
