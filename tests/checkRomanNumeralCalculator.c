a4963bb812e536d44e8777f9561395360f35b4e9
Tasewell Fox <tfox@pillartechnology.com>
2016-09-01 23:55:44 -0500

    red test II+I=III

--- tests/checkRomanNumeralCalculator.c
+++ tests/checkRomanNumeralCalculator.c
@@ -13,6 +13,20 @@ START_TEST(test_I_plus_I_equals_II)
 }
 END_TEST
 
+START_TEST(test_II_plus_I_equals_III)
+{
+  const char* operand1 = "II";
+  const char* operand2 = "I";
+
+  char result[10];
+
+  add_roman_numeral(operand1, operand2, result);
+
+  ck_assert_str_eq(result, "III");
+}
+END_TEST
+
+
 START_TEST(test_IV_plus_I_equals_V)
 {
   const char* operand1 = "IV";
@@ -79,6 +93,7 @@ Suite * calculator_suite(void)
   tc_core = tcase_create("Core");
 
   tcase_add_test(tc_core, test_I_plus_I_equals_II);
+  tcase_add_test(tc_core, test_II_plus_I_equals_III);
   tcase_add_test(tc_core, test_IV_plus_I_equals_V);
   tcase_add_test(tc_core, test_V_plus_I_equals_VI);
   tcase_add_test(tc_core, test_successful_add_returns_OK);
