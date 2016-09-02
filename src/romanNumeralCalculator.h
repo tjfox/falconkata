ca9f926707b79234f82783fbdcf84e180d37c61b
Tasewell Fox <tfox@pillartechnology.com>
2016-09-01 21:42:01 -0500

    Remove lowercase test. Red test for successful retval

--- src/romanNumeralCalculator.h
+++ src/romanNumeralCalculator.h
@@ -1,7 +1,12 @@
 #ifndef ROMANNUMERALCALCULATOR_H
 #define ROMANNUMERALCALCULATOR_H
 
-void add_roman_numeral(const char* operand1,
+enum op_success_t
+{
+  OK
+};
+
+int add_roman_numeral(const char* operand1,
   const char* operand2, char* result);
 
 #endif /* ROMANNUMERALCALCULATOR_H */
--- tests/checkRomanNumeralCalculator.c
+++ tests/checkRomanNumeralCalculator.c
@@ -13,6 +13,17 @@ START_TEST(test_I_plus_I_equals_II)
 }
 END_TEST
 
+START_TEST(test_successful_operation_returns_OK)
+{
+  const char * operand1 = "I";
+  char result[10];
+
+  int success = add_roman_numeral(operand1, operand1, result);
+
+  ck_assert_int_eq(success, OK);
+}
+END_TEST
+
 Suite * calculator_suite(void)
 {
   Suite *s;
@@ -22,6 +33,7 @@ Suite * calculator_suite(void)
   tc_core = tcase_create("Core");
 
   tcase_add_test(tc_core, test_I_plus_I_equals_II);
+  tcase_add_test(tc_core, test_successful_operation_returns_OK);
   suite_add_tcase(s, tc_core);
 
   return s;
@@ -36,7 +48,7 @@ int main(void)
     s = calculator_suite();
     sr = srunner_create(s);
 
-    srunner_run_all(sr, CK_NORMAL);
+    srunner_run_all(sr, CK_VERBOSE);
     number_failed = srunner_ntests_failed(sr);
     srunner_free(sr);
     return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
